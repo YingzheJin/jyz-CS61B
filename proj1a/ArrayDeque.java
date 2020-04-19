f9a5c3d25c8363aef7030d1e6e92777fccfed1d4
YingzheJin <63117938+YingzheJin@users.noreply.github.com>
2020-04-20 02:47:11 +0800

    4

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,34 +3,35 @@ public class ArrayDeque<T> {
     private int size;
     private int nextFirst;
     private int nextLast;
-    private int length;
 
     /** Creates an empty list. */
     public ArrayDeque() {
         items = (T[]) new Object[8];
-        length = 8;
         size = 0;
         nextFirst = 4;
         nextLast = 5;
     }
 
+    private  boolean isSparse() {
+        return items.length >= 16 && size <= items.length * 0.25;
+    }
+
     private int minusOne(int index) {
-        return (index - 1 + length) % length;
+        return (index - 1 + items.length) % items.length;
     }
     private int plusOne(int index) {
-        return (index + 1 + length) % length;
+        return (index + 1 + items.length) % items.length;
     }
 
     private void resize(int capacity) {
         T[] a = (T[]) new Object[capacity];
-        length = capacity;
         for (int i = nextFirst + 1, j = 0; i < nextLast; i = plusOne(i), j++) {
             a[j] = items[i];
         }
         items = a;
     }
     public void addFirst(T x) {
-        if (size == length) {
+        if (size == items.length) {
             resize(size * 2);
         }
 
@@ -40,7 +41,7 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T x) {
-        if (size == length) {
+        if (size == items.length) {
             resize(size * 2);
         }
 
@@ -64,6 +65,9 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
+        if (isSparse()) {
+            resize(items.length / 2);
+        }
         if (isEmpty()) {
             return null;
         }
@@ -75,6 +79,9 @@ public class ArrayDeque<T> {
     }
 
     public T removeLast() {
+        if (isSparse()) {
+            resize(items.length / 2);
+        }
         if (isEmpty()) {
             return null;
         }
@@ -86,10 +93,11 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index) {
-        int p = plusOne(nextFirst);
-        for (int i = 0; i < index; i++) {
-            p = plusOne(p);
+        if (index >= size) {
+            return null;
         }
-        return items[p];
+
+         return items[plusOne(nextFirst + index)];
+
     }
 }
